b119c05bff8b043de4e8d57c3c9610318b58cca2
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-10-05 23:57:26 +0900

    not working

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ const server = http.createServer((req, res) => {
 
   fs.writeFileSync("message.txt", "DUMMY message");
 
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first page</title></head>");
-  res.write("<body><h1>Hello world from node!</h1></body>");
-  res.write("</html>");
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<html>");
+  // res.write("<head><title>My first page</title></head>");
+  // res.write("<body><h1>Hello world from node!</h1></body>");
+  // res.write("</html>");
 
   res.end();
 });
