50d1def30a1b66ba60fd99883c4bacd7ca95bb08
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-10-05 23:47:03 +0900

    rendering the content for / request

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first page</title></head>");
-  res.write("<body><h1>Hello world from node!</h1></body>");
-  res.write("</html>");
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<html>");
+  // res.write("<head><title>My first page</title></head>");
+  // res.write("<body><h1>Hello world from node!</h1></body>");
+  // res.write("</html>");
 
   res.end();
 });
